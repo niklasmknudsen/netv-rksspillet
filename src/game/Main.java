e569c7205ade2907599321638864b2420428d068
nikla <nk@komunikado.dk>
2020-10-06 23:33:38 +0200

    [changed] Error problem with Thread-4 reason: new dialog window

--- src/game/Main.java
+++ src/game/Main.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -40,6 +41,8 @@ public class Main extends Application {
 	public static Player me;
 	/*public static List<Player> players = new ArrayList<Player>(); */
 	public static Common common;
+	
+	public static SignInDialog signInDialog;
 	
 	private Label[][] fields;
 	private TextArea scoreList;
@@ -106,7 +109,7 @@ public class Main extends Application {
 			Scene scene = new Scene(grid,scene_width,scene_height);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-
+			
 			scene.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
 				switch (event.getCode()) {
 				case UP:    playerMoved(0,-1,"up");    break;
@@ -117,62 +120,41 @@ public class Main extends Application {
 				}
 			});
 			
-            // Setting up standard players
-			pair p=getRandomFreePosition();
-			me = new Player("Orville",p.getX(),p.getY(),"up");
-			//players.add(me);
-			common.addPlayer(me);
-			fields[p.getX()][p.getY()].setGraphic(new ImageView(hero_up));
-
-			p=getRandomFreePosition();
+			
+			pair p = getRandomFreePosition();
 			Player harry = new Player("Harry",p.getX(),p.getY(),"up");
 			//players.add(harry);
-			common.addPlayer(harry);
+			Common.addPlayer(harry);
 			fields[p.getX()][p.getY()].setGraphic(new ImageView(hero_up));
-
-			scoreList.setText(getScoreList());
 			
+		
+			
+			scoreList.setText(getScoreList());
+			openLoginScreen();
 			connectToServer();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	public void openLoginScreen() {
+		Platform.runLater(() -> {
+			me = new Player();
+			signInDialog = new SignInDialog(me);
+			signInDialog.showAndWait();
+		});
+	}
+	
 	public void connectToServer() {
-		Socket clientSocket;
-		String sentence;
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		Socket connectionSocket;
 		try {
-			clientSocket = new Socket("10.24.67.16",6900);
-			DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			//while (true) {
+			connectionSocket = new Socket("192.168.87.164",6900);
+			DataOutputStream outToServer = new DataOutputStream(connectionSocket.getOutputStream());
+			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 			
-				sentence = inFromUser.readLine();
-				outToServer.writeBytes(sentence + '\n'); // sends user name to server
-				
-				System.out.println(inFromServer.readLine());
-				
-				// common get players
-				String response = inFromServer.readLine();
-				String[] commands = response.split(",");
-				System.out.println(response);
-				
-				try {
-					String playerName = commands[0];
-					int playerPositionX = Integer.parseInt(commands[1]);
-					int playerPositionY = Integer.parseInt(commands[2]);
-					String playerDirection = commands[3];
-					
-					this.movePlayerOnScreen(1,4, playerPositionX, playerPositionY, playerDirection);
-					
-					System.out.println();	
-				} catch (NumberFormatException error) {
-					error.printStackTrace();
-				}
-				
-				clientSocket.close();
-			//}
+			ClientThread ct = new ClientThread(connectionSocket, inFromServer, outToServer);
+			ct.start();
+			
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -223,9 +205,7 @@ public class Main extends Application {
 			if (direction.equals("down")) {
 				fields[newx][newy].setGraphic(new ImageView(hero_down));
 			};
-			});
-
-		
+      	});
 	}
 	
 
@@ -262,7 +242,7 @@ public class Main extends Application {
 	{
 		Platform.runLater(() -> {
 			scoreList.setText(getScoreList());
-			});
+		});
 	}
 	public void playerMoved(int delta_x, int delta_y, String direction) {
 		updatePlayer(delta_x,delta_y,direction);
@@ -288,6 +268,61 @@ public class Main extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	class ClientThread extends Thread {
+		private Socket connectionSocket;
+		private BufferedReader inFromServer;
+		private DataOutputStream outToServer;
+		
+		public ClientThread(Socket connectionSocket, BufferedReader inFromServer, DataOutputStream outToServer) {
+			this.connectionSocket = connectionSocket;
+			this.inFromServer = inFromServer;
+			this.outToServer = outToServer;
+		}
+		
+		@Override
+		public void run() {
+			String sentence;
+			//BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+			try {
+
+				while (true) {
+					// Setting up standard players
+					pair p = getRandomFreePosition();
+					Common.addPlayer(me);
+					fields[p.getX()][p.getY()].setGraphic(new ImageView(hero_up)); 
+					
+					// common get players
+					String response = inFromServer.readLine();
+					String[] commands = response.split(",");
+					System.out.println(response); 
+					
+					try {
+					//	String playerName = commands[0];
+						int playerPositionX = Integer.parseInt(commands[1]);
+						int playerPositionY = Integer.parseInt(commands[2]);
+						String playerDirection = commands[3];
+						
+						
+						
+						
+						Thread.sleep(2000);
+					} catch (NumberFormatException error) {
+						error.printStackTrace();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					
+					connectionSocket.close();
+				}
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
 	}
 }
 
